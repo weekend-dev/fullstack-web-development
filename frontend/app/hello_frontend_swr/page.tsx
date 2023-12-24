157c3ed67e4d9d48234500670119bec52941c6af
yu ishiguro <yu-ishiguro@yunoMacBook-Air.local>
2023-12-24 09:59:10 +0700

    add chapter4_done

---
+++ frontend/app/hello_frontend_swr/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import useSWR from 'swr'
+
+const fetcher = (url: string) => fetch(url).then(res => res.json())
+
+export default function Page() {
+    const { data, error } = useSWR('/api/hello', fetcher)
+
+    if (error) return <div>failed to load</div>
+    if (!data) return <div>loading...</div>
+
+    return <div>hello {data.name}!</div>
+}
---
+++ frontend/app/hello_without_jsx/page.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+export default function Page() {
+    return React.createElement('h1', null, `Hello, Next.js!`);
+}
