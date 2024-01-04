88e924e49c70497c6d657c308866905a70d45f6d
ishigero <yuu.ishiguro@gmail.com>
2024-01-04 14:56:07 +0700

    sample GET api http://localhost:8000/api/hello/backend/

---
+++ backend/api/hello/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path("backend/", views.Backend.as_view())
+]
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('api/hello_db/', include('api.hello_db.urls')),
+    path('api/hello/', include('api.hello.urls')),
 ]
